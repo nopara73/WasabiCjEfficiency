8ce9055477a8d9b1c914af2ea7b1ed2a3240ca78
nopara73 <adam.ficsor73@gmail.com>
2020-01-20 13:47:42 -0400

    Add some helpers

--- WasabiCjEfficiency/Program.cs
+++ WasabiCjEfficiency/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using NBitcoin;
+using NBitcoin.RPC;
+using System;
 using System.IO;
+using System.Net;
+using WasabiCjEfficiency.Helpers;
 
 namespace WasabiCjEfficiency
 {
@@ -7,7 +11,50 @@ namespace WasabiCjEfficiency
     {
         static void Main(string[] args)
         {
+            Logger.InitializeDefaults();
+
+            ParseArgs(args, out NetworkCredential rpcCred);
+
+            var rpcConf = new RPCCredentialString
+            {
+                UserPassword = rpcCred
+            };
+            var client = new RPCClient(rpcConf, Network.Main);
+
             File.ReadAllLines("CoinJoinsMain.txt");
+
+            using (BenchmarkLogger.Measure(operationName: "Parsing The Blockchain"))
+            {
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press a button to exit...");
+            Console.ReadKey();
+        }
+
+        private static void ParseArgs(string[] args, out NetworkCredential cred)
+        {
+            string rpcUser = null;
+            string rpcPassword = null;
+
+            var rpcUserArg = "--rpcuser=";
+            var rpcPasswordArg = "--rpcpassword=";
+            foreach (var arg in args)
+            {
+                var idx = arg.IndexOf(rpcUserArg, StringComparison.Ordinal);
+                if (idx == 0)
+                {
+                    rpcUser = arg.Substring(idx + rpcUserArg.Length);
+                }
+
+                idx = arg.IndexOf(rpcPasswordArg, StringComparison.Ordinal);
+                if (idx == 0)
+                {
+                    rpcPassword = arg.Substring(idx + rpcPasswordArg.Length);
+                }
+            }
+
+            cred = new NetworkCredential(rpcUser, rpcPassword);
         }
     }
 }
