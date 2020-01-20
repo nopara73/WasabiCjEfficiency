278ff1ad8096ade82b41d0151483bc2e63abd889
nopara73 <adam.ficsor73@gmail.com>
2020-01-20 14:15:59 -0400

    filter for confirmed txs

--- WasabiCjEfficiency/CoinJoinIndexer.cs
+++ WasabiCjEfficiency/CoinJoinIndexer.cs
@@ -12,19 +12,35 @@ namespace WasabiCjEfficiency
     public class CoinJoinIndexer
     {
         private RPCClient Client { get; }
+        private uint256[] coinJoinHashes;
 
         public CoinJoinIndexer(RPCClient client)
         {
             Client = client;
+            coinJoinHashes = File.ReadAllLines("CoinJoinsMain.txt").Select(x => new uint256(x)).ToArray();
         }
 
         internal async Task<IEnumerable<CoinJoinDay>> GetDailyStatsAsync()
         {
-            var allCoinJoinHashes = (await File.ReadAllLinesAsync("CoinJoinsMain.txt")).Select(x => new uint256(x));
+            Console.WriteLine($"{coinJoinHashes.Length} coinjoins will be analyzed.");
+
+            var days = new List<CoinJoinDay>();
+            var daysLock = new object();
+
+            foreach(var cjHash in coinJoinHashes)
+            {
+                var txi = await Client.GetRawTransactionInfoAsync(cjHash);
+                if (txi.Confirmations == 0) continue;
 
 
+            }
 
-            return new CoinJoinDay[] { };
+            return days;
+        }
+
+        public bool IsCoinJoin(uint256 txid)
+        {
+            return coinJoinHashes.Contains(txid);
         }
     }
 }
