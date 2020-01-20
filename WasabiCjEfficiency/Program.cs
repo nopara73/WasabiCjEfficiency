e17e517b2834942880cb76d71714476dcb0dba83
nopara73 <adam.ficsor73@gmail.com>
2020-01-20 13:42:37 -0400

    Add coinjoins

--- WasabiCjEfficiency/Program.cs
+++ WasabiCjEfficiency/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace WasabiCjEfficiency
 {
@@ -6,7 +7,7 @@ namespace WasabiCjEfficiency
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            File.ReadAllLines("CoinJoinsMain.txt");
         }
     }
 }
